f7aeb5436bd99a6c8eaa2210a3d8067c7bfcf40c
SinghCoder <harpinderjot36@gmail.com>
2023-08-19 19:07:57 +0530

    print emails also

--- capturers/capturer.py
+++ capturers/capturer.py
@@ -15,10 +15,12 @@ slack_client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
 
 app = Flask(__name__)
 
-def get_user_name(user_id):
+def get_user_info(user_id):
     try:
         user_info = slack_client.users_info(user=user_id)
-        return user_info['user']['real_name']
+        user_name = user_info['user']['real_name']
+        user_email = user_info['user']['profile']['email']
+        return user_name, user_email
     except SlackApiError:
         print(f"Error getting user info: {traceback.format_exc()}")
         return user_id
@@ -39,8 +41,8 @@ def resolve_mentions(message_text):
     user_mentions = re.findall(r'<@(\w+)>', message_text)
     resolved_message = message_text
     for mention in user_mentions:
-        username = get_user_name(mention)
-        resolved_message = resolved_message.replace(f'<@{mention}>', f'{username}')
+        user_name, user_email = get_user_info(mention)
+        resolved_message = resolved_message.replace(f'<@{mention}>', f'{user_name} - {user_email}')
     return resolved_message
 
 
@@ -56,7 +58,7 @@ def slack_events():
     # Print only the message events:
     if event_type == 'message':
         user_id = data['event']['user']
-        user_name = get_user_name(user_id)
+        user_name, user_email = get_user_info(user_id)
 
         channel_id = data['event']['channel']
         channel_name = get_channel_name(channel_id)
@@ -65,7 +67,7 @@ def slack_events():
         resolved_message = resolve_mentions(message_text)
 
         # Format: User <user_name> sent message: <message_text> in channel <channel_name>
-        content = f"User {user_name} sent message: {resolved_message} in channel {channel_name}"
+        content = f"User {user_name} - {user_email} sent message: {resolved_message} in channel {channel_name}"
         print(content)
         if my_slack_user_name in content:
             print("This message mentions me")
@@ -76,6 +78,7 @@ def slack_events():
 @app.route('/telegram', methods=['POST'])
 def telegram_events():
     data = request.json
+    print(data)
     if 'message' in data and 'text' in data['message']:
         username = data['message']['from']['first_name']
         text = data['message'].get("text", "No text")
