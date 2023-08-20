43c19d37fcbf1298fd0a4d2129b0d8d3af470046
Rahul Jain <rahul.jain@devrev.ai>
2023-08-20 19:06:37 +0530

    find conflicting events

--- distill.py
+++ distill.py
@@ -1,9 +1,11 @@
 import os
+from datetime import datetime
+import dateutil.parser as parser
 from langchain.agents import AgentExecutor, OpenAIFunctionsAgent
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import SystemMessage
 
-from tools.calendar import create_calendar_event, get_calendar_events
+from tools.calendar import create_calendar_event, get_calendar_events,fetch_calendar_events
 from tools.time import Datetime
 from tools.todo import add_todo_item
 from tools.notes import add_notes
@@ -31,4 +33,35 @@ def distill_agent_executor():
     agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
     return agent_executor
 
+
+def get_conflicting_meetings():
+    events = fetch_calendar_events()
+    if not events:
+        return "No events found for today"
+    event_summary = []
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        start = parser.isoparse(start)
+        end = event['end'].get('dateTime', event['end'].get('date'))
+        end = parser.isoparse(end)
+        event_summary = event_summary + [{
+            'title': event['summary'],
+            'start': start,
+            'end': end,
+        }]
+
+    conflicts = []
+    event_summary.sort(key=lambda x: x['start'])
+    for i in range(len(event_summary) - 1):
+        if event_summary[i]['end'] > event_summary[i + 1]['start']:
+            conflicts = conflicts + [(event_summary[i], event_summary[i + 1])]
+    
+    if not conflicts:
+        return "No conflicts found for today"
+    else:
+        result = "Conflicts found for today: \n"
+        for e1, e2 in conflicts:
+            edesc = lambda e : f"{e['title']} at {e['start'].strftime('%H:%M')}"
+            result += f"{edesc(e1)} conflicts with {edesc(e2)}. \n"
+        return result
 
